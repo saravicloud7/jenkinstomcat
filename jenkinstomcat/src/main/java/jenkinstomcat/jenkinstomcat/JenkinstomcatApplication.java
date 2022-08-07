0a876ec7e4a57885e94ffa0f020b5fa24168fefb
Saravi <Saravi@DESKTOP-UB55E0G>
2022-08-07 10:06:42 +0530

    test commit

---
+++ jenkinstomcat/src/main/java/jenkinstomcat/jenkinstomcat/JenkinstomcatApplication.java
@@ -0,0 +1,13 @@
+package jenkinstomcat.jenkinstomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinstomcatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinstomcatApplication.class, args);
+	}
+
+}
